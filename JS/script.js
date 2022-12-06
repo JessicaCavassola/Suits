df92781b0df700141c22cd8590dd20979215e871
Jessica Cavassola <jcavassola@gmail.com>
2022-12-06 16:03:59 -0300

    primeiro commit

---
+++ JS/script.js
@@ -0,0 +1,79 @@
+//SELEÇÃO DE ELEMENTOS
+const menuBtn= document.querySelector("#menu") //hamburger
+const closeMenuBtn = document.querySelector ("#close-menu")
+const menu = document.querySelector ("#mobile-navbar")
+
+const desktopLinks = document.querySelectorAll ("#navbar a") //seleciona tds elementos do msm padrão de seletor ou seja (nav bar a)
+const mobileLinks = document.querySelectorAll("#mobile-navbar a")
+const allLinks = [...desktopLinks, ...mobileLinks]
+const slides = document.querySelectorAll(".banner")
+const dots = document.querySelectorAll (".dot")
+let slideIndex = 0;
+
+
+//FUNÇOES
+function smoothScroll(e){ 
+    e.preventDefault();
+    const href = this.getAttribute("href")
+    const offsetTop = document.querySelector(href).offsetTop
+    console.log(href)
+    
+scroll({
+    top: offsetTop,
+    behavior: "smooth",
+  })
+
+  setTimeout(() => {
+    if (menu.classList.contains("menu-active")) {
+      menu.classList.remove("menu-active");
+    }
+  }, 500);
+}
+
+function showSlides(){
+   
+    for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active")
+    dots[i].classList.remove("active")
+}
+slideIndex++;
+
+if (slideIndex > slides.Length){
+    slideIndex = 1
+}
+
+slides[slideIndex - 1].classList.add("active");
+dots[slideIndex - 1].classList.add("active");
+
+setTimeout(showSlides, 3000)
+}
+
+//EVENTOS
+
+//Inicializacao
+  showSlides()
+
+
+//abertura e fechamento menu mobile                                          
+menuBtn.addEventListener ("click", (e) => { //ARROW FUNCTION
+    menu.classList.add("menu-active");
+
+})
+
+closeMenuBtn.addEventListener ("click", (e) => { //ARROW FUNCTION            
+    menu.classList.remove("menu-active");
+    
+}) 
+//ou  |
+//    v
+/*[menuBtn, closeMenuBtn].forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      menu.classList.toggle("menu-active");
+    });
+  }); */
+
+allLinks.forEach((link) => {
+link.addEventListener("click",smoothScroll)
+
+  }) 
+  
